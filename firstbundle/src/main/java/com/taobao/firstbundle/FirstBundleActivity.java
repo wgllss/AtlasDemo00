6d47470dc929060dd2d3543242b8b559afe1d14a
王贵 <283495283@qq.commkdir>
2017-10-17 16:00:34 +0800

    Initial commit

---
+++ firstbundle/src/main/java/com/taobao/firstbundle/FirstBundleActivity.java
@@ -0,0 +1,33 @@
+package com.taobao.firstbundle;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.taobao.firstbundle.fragment.BlankFragment;
+import com.taobao.publicBundle.Tools;
+
+public class FirstBundleActivity extends AppCompatActivity implements BlankFragment.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firstbundle);
+
+//        bundleCompile
+        Log.e("providedComple awb", "invoke tools at public bundle  Tools.getCurrentTime() > " + Tools.getCurrentTime());
+
+//        Toast.makeText(this,"dsfsfs",Toast.LENGTH_LONG).show();
+    }
+
+//    @Override
+//    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+//
+//    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
